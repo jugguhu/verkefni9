d0e0ff1a3c561e4fa80ba0309397d69e2a29d0c8
Jón Gunnar Hannesson <jgh060@gmail.com>
2019-11-14 14:28:47 +0000

    added formHandler, fetchResults and to init

--- scripts.js
+++ scripts.js
@@ -4,8 +4,38 @@ const API_URL = 'https://apis.is/company?name=';
  * Leit að fyrirtækjum á Íslandi gegnum apis.is
  */
 const program = (() => {
-  function init(companies) {
 
+  let input;
+  function init(companies) {
+    console.log(companies);
+    input = companies.querySelector("input");
+    const form = companies.querySelector("form");
+    form.addEventListener('submit', formHandler);
+
+  }
+
+  function fetchCompanies(company){
+    fetch(`${API_URL}${company}`)
+      .then((result) =>{
+        if(!result.ok){
+          throw new error('Villa við að sækja gögn');
+        }
+        return result.json();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }
+
+  function formHandler(e) {
+    e.preventDefault();
+    const value = input.value;
+    input.value = "";
+    if(!value || !value.trim()){
+    console.log('value error');
+    }
+    else{
+      fetchCompanies(value);
+    }
   }
 
   return {
@@ -14,5 +44,6 @@ const program = (() => {
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
+  const companies = document.querySelector("main");
   program.init(companies);
 });
